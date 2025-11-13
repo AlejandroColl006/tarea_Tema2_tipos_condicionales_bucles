8af0bd5516654c94ad4f500346facfcb32930d6b
Your Name <acolllen@myuax.com>
2025-11-13 18:09:48 +0100

    Este guardado (save), debería ir por delante del guardado del CMakeList, pero al intentar compilar el código me daba fallo por lo que es la razón por la que tenía que crear el CMakeList.txt, además me equivoqué a la hora escribir el nombre de la librería.

--- primerPrograma.c
+++ primerPrograma.c
@@ -1,6 +1,16 @@
-#include <studio.h>
+#include <stdio.h>
 
 int main () {
-  int suma = 0;
-  int contador = 0
-      }
+    int suma = 0;
+    int contador = 0;
+
+    for (int i = 1; i <= 100; i++) {
+        if (i % 2 != 0) {
+            suma += i;
+            contador++;
+        }
+    }
+    printf("La suma de todos los números impares del 1 al 100 es: %d", suma);
+    printf("Contando la cantidad de números impares, nos da el resultado de: %d", contador);
+    return 0;
+}
